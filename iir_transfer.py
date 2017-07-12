5030c5c6cae8802c1d018c82da2411dd0da11261
Robert Jordens <jordens@gmail.com>
2017-07-12 10:23:03 +0200

    transfer: cleanup, add PI example

--- iir_transfer.py
+++ iir_transfer.py
@@ -44,33 +44,53 @@ def analyze(x, y, log=False):
     x = x - np.mean(x)
     y = y - np.mean(y)
     n = len(x)
-    w = np.hanning(n)
-    x = (x.reshape(-1, n)*w).sum(0)
-    y = (y.reshape(-1, n)*w).sum(0)
+    # w = np.hanning(n)
+    # x = (x.reshape(-1, n)*w).sum(0)
+    # y = (y.reshape(-1, n)*w).sum(0)
     t = (np.fft.rfft(y)/np.fft.rfft(x))
-    f = np.fft.rfftfreq(n)*2
+    f = np.fft.rfftfreq(n)
     fmin = f[1]
     ax[1].plot(f,  20*np.log10(np.abs(t)), "r-")
-    ax[1].set_ylim(-70, 3)
-    ax[1].set_xlim(fmin, 1.)
+    # ax[1].set_ylim(-70, 3)
+    ax[1].set_xlim(fmin, .5)
     if log:
         ax[1].set_xscale("log")
-    ax[1].set_xlabel("frequency (fs/2)")
+    ax[1].set_xlabel("frequency (fs)")
     ax[1].set_ylabel("magnitude (dB)")
     ax[1].grid(True)
     ax[2].plot(f,  np.rad2deg(np.angle(t)), "r-")
-    ax[2].set_xlim(fmin, 1.)
+    ax[2].set_xlim(fmin, .5)
     if log:
         ax[2].set_xscale("log")
-    ax[2].set_xlabel("frequency (fs/2)")
+    ax[2].set_xlabel("frequency (fs)")
     ax[2].set_ylabel("phase (deg)")
     ax[2].grid(True)
     return fig
 
 
+def f2i(f, width, shift):
+    i = int(round(f*(1 << shift)))
+    k = 1 << width - 1
+    assert -k <= i <= k - 1, (f, width, shift, hex(i), hex(k))
+    return i
+
+
+def pi_iir(widths, f, k, g=np.inf):
+    # transfer function: H(s) = k*(s/(2*pi*f) + 1)/(s/(2*pi*f) + 1/g)
+    # f: integrator corner a.k.a. P zero (in units of fs)
+    # k: P gain
+    # g: I gain limit
+    f *= np.pi
+    z = f/g + 1
+    a1 = f2i((f/g - 1)/z, widths.coeff, widths.shift)
+    b0 = f2i(k*(f + 1)/z, widths.coeff, widths.shift + 1)  # pre-adder
+    b1 = f2i(k*(f - 1)/z, widths.coeff, widths.shift + 1)  # pre-adder
+    return a1, b0, b1
+
+
 def main():
     w = iir.IIRWidths(state=25, coeff=18, adc=16,
-            asf=14, word=16, accu=48, shift=16,
+            asf=14, word=16, accu=48, shift=11,
             channel=1, profile=1)
     dut = iir.IIR(w)
 
@@ -83,21 +103,29 @@ def main():
         yield dut.ctrl[ch].profile.eq(pr)
         yield from dut.set_state(ch, 0, coeff="x1")
         yield from dut.set_state(ch, 0, coeff="x0")
-        y1 = 1 << w.state - 2
         a1 = -(1 << w.coeff - 3)
-        b0 = 1 << w.coeff - 2
+        b0 = 1 << w.coeff - 3
+        b1 = b0
         offset = 1 << w.coeff - 2
+        y1 = offset
+
+        # a1, b0, b1 = pi_iir(w, f=.2, k=.1)
+        # a1, b0, b1 = pi_iir(w, f=.3, k=15.9)
+        a1, b0, b1 = pi_iir(w, f=.005, k=.01)
+        y1 = 1 << w.state - 2
+        offset = 0
+        # print([hex(_) for _ in (offset, y1, a1, b0, b1)])
         yield from dut.set_state(ch, y1, profile=pr, coeff="y1")
         yield from dut.set_coeff(ch, pr, "cfg",
                         (ch << 0) | (0 << 8))
         yield from dut.set_coeff(ch, pr, "a1", a1)
         yield from dut.set_coeff(ch, pr, "b0", b0)
-        yield from dut.set_coeff(ch, pr, "b1", 0)
+        yield from dut.set_coeff(ch, pr, "b1", b1)
         yield
         yield from dut.set_coeff(ch, pr, "offset", offset)
         yield
 
-    x, y = drive_uni(dut, samples=1<<6, amplitude=.3, seed=0x123,
+    x, y = drive_uni(dut, samples=1<<8, amplitude=.2, seed=0x123,
             cfg=cfg(dut))
     analyze(x, y, log=True)
     plt.show()
