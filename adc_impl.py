555cb0ac8490a10b922f63a3d285069d05777cd7
Robert Jordens <jordens@gmail.com>
2017-07-15 19:22:21 +0200

    adc_impl, iir_impl: use harness

--- adc_impl.py
+++ adc_impl.py
@@ -2,66 +2,22 @@ from migen import *
 from migen.build.generic_platform import *
 from migen.genlib import io
 
-import adc_ser, arty
+import adc_ser, arty, impl
 
 
-class Top(Module):
+class Top(impl.Impl):
     def __init__(self, plat):
-        # build a makeshift ADC interface out of PMODs
-        plat.constraint_manager.available.append(
-                ("adc_ser", 0,
-                    Subsignal("sck_p", Pins("E15"), IOStandard("LVDS_25")),
-                    Subsignal("sck_n", Pins("E16"), IOStandard("LVDS_25")),
-
-                    # clock-capable!
-                    Subsignal("clkout_p", Pins("D15"), IOStandard("LVDS_25")),
-                    Subsignal("clkout_n", Pins("C15"), IOStandard("LVDS_25")),
-
-                    Subsignal("cnv_b", Pins("T10"), IOStandard("LVCMOS25")),
-
-                    Subsignal("sdoa_p", Pins("U12"), IOStandard("LVDS_25")),
-                    Subsignal("sdoa_n", Pins("V12"), IOStandard("LVDS_25")),
-                    Subsignal("sdob_p", Pins("V10"), IOStandard("LVDS_25")),
-                    Subsignal("sdob_n", Pins("V11"), IOStandard("LVDS_25")),
-                    Subsignal("sdoc_p", Pins("U14"), IOStandard("LVDS_25")),
-                    Subsignal("sdoc_n", Pins("V14"), IOStandard("LVDS_25")),
-                    Subsignal("sdod_p", Pins("T13"), IOStandard("LVDS_25")),
-                    Subsignal("sdod_n", Pins("U13"), IOStandard("LVDS_25")),
-                )
-        )
-
-        clk = plat.request(plat.default_clk_name)
-        self.submodules += io.CRG(clk)
-
+        super().__init__(plat, clk=250e6)
         params = adc_ser.ADCParams(width=16, channels=8,
                 t_cnvh=4, t_conv=57, t_rtt=4)
         self.submodules.adc = adc = adc_ser.ADC(
                 plat.request("adc_ser"), params)
 
-        outs = adc.data
-        sr1 = Signal(len(Cat(outs)), reset_less=True)
-        led = plat.request("user_led")
-        but = plat.request("user_btn")
-        x = Signal(reset_less=True)
-        self.sync += [
-                x.eq(but),
-
-                adc.start.eq(x),
-
-                led.eq(sr1[0]),
-                sr1.eq(sr1[1:]),
-                If(adc.done,
-                    sr1.eq(Cat(outs)),
-                ),
-        ]
-
-
-def main():
-    plat = arty.Platform()
-    plat.default_clk_period = 1000/250
-    top = Top(plat)
-    plat.build(top)
+        self.dummy_inputs([adc.start], 1)
+        self.dummy_outputs([adc.done, adc.reading] + adc.data, adc.done)
 
 
 if __name__ == "__main__":
-    main()
+    plat = arty.Platform()
+    top = Top(plat)
+    plat.build(top)
--- iir_impl.py
+++ iir_impl.py
@@ -1,10 +1,10 @@
 from migen import *
 
-import iir, arty
+import iir, arty, impl
 
-
-class Top(Module):
+class Top(impl.Impl):
     def __init__(self, plat):
+        super().__init__(plat, clk=150e6)
         w = iir.IIRWidths(state=25, coeff=18, adc=16,
                 asf=14, word=16, accu=48, shift=11,
                 channel=3, profile=5)
@@ -16,48 +16,20 @@ class Top(Module):
         m_state = top.m_state.get_port(write_capable=True)
         self.specials += m_coeff, m_state
 
-        # wire dummy SRs to inputs and outputs to prevent optimization
-
         ins = [
                 m_state.adr, m_state.we, m_state.dat_w,
                 m_coeff.adr, m_coeff.we, m_coeff.dat_w,
-                ] + top.adc
+                top.start] + top.adc
         for ctrl in top.ctrl:
             ins += ctrl.flatten()
+        self.dummy_inputs(ins, top.start)
 
         outs = [top.shifting, top.loading, top.processing, top.done,
                 m_state.dat_r, m_coeff.dat_r] + top.dds
-
-        sr0 = Signal(len(Cat(ins)), reset_less=True)
-        sr1 = Signal(len(Cat(outs)), reset_less=True)
-        led = plat.request("user_led")
-        but = plat.request("user_btn")
-        x = Signal(reset_less=True)
-        self.sync += [
-                x.eq(but),
-
-                top.start.eq(x),
-
-                sr0[0].eq(x),
-                sr0[1:].eq(sr0),
-                If(top.done,
-                    Cat(ins).eq(sr0)
-                ),
-
-                led.eq(sr1[0]),
-                sr1.eq(sr1[1:]),
-                If(top.done,
-                    sr1.eq(Cat(outs)),
-                ),
-        ]
-
-
-def main():
-    plat = arty.Platform()
-    plat.default_clk_period = 1000/150
-    top = Top(plat)
-    plat.build(top)
+        self.dummy_outputs(outs, top.done)
 
 
 if __name__ == "__main__":
-    main()
+    plat = arty.Platform()
+    top = Top(plat)
+    plat.build(top)
