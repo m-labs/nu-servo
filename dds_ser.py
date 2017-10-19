99a884ea539dd091c2760166136b830f8d21dc23
Robert Jordens <jordens@gmail.com>
2017-10-19 10:30:12 +0200

    dds_ser: implement loading and io_update

--- dds_ser.py
+++ dds_ser.py
@@ -11,18 +11,22 @@ SPIParams = spi.SPIParams
 class SPIDDS(spi.SPISimple):
     def __init__(self, pads, params):
         super().__init__(pads, params)
-        io_update = self._diff(pads, "io_update", output=True)
 
+        self.profile = [Signal(32 + 16 + 16, reset_less=True)
+                for i in range(params.channels)]
+        cmd = Signal(8, reset=0x0e)  # write to single tone profile 0
+        self.sync += [
+                If(self.start,
+                    [d.eq(Cat(p, cmd))
+                        for d, p in zip(self.data, self.profile)]
+                )
+        ]
+
+        io_update = self._diff(pads, "io_update", output=True)
+        # this assumes that the cycle time (1/125 MHz = 8 ns) is >1 SYNC_CLK
+        # cycle (1/250 MHz = 4ns)
         done, self.done = self.done, Signal()
         self.sync += [
-                If(~done,
-                    self.done.eq(0),
-                ),
-                If(done,
-                    io_update.eq(1),
-                ),
-                If(io_update,
-                    io_update.eq(0),
-                    self.done.eq(1)
-                )
+                self.done.eq(done),
+                io_update.eq(done & ~self.done)
         ]
