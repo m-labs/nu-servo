1f9664fb58340b304917adea623719af42378ca4
Robert Jordens <jordens@gmail.com>
2017-10-20 12:06:51 +0200

    servo_impl: update

--- servo_impl.py
+++ servo_impl.py
@@ -8,10 +8,10 @@ import adc_ser, iir, dds_ser, impl
 class Top(impl.Impl):
     def __init__(self, plat):
         super().__init__(plat, clk=150e6)
-        params = adc_ser.ADCParams(width=16, channels=8, lanes=4,
+        adc_p = adc_ser.ADCParams(width=16, channels=8, lanes=4,
                 t_cnvh=4, t_conv=57, t_rtt=4)
         adc_pads = plat.request("adc_ser")
-        self.submodules.adc = adc = adc_ser.ADC(adc_pads, params)
+        self.submodules.adc = adc_ser.ADC(adc_pads, adc_p)
 
         plat.add_period_constraint(adc_pads.clkout_p,
                 plat.default_clk_period)
@@ -19,45 +19,71 @@ class Top(impl.Impl):
                 plat.lookup_request(plat.default_clk_name),
                 adc_pads.clkout_p)
 
-        w = iir.IIRWidths(state=25, coeff=18, adc=16,
+        proc_p = iir.IIRWidths(state=25, coeff=18, adc=16,
                 asf=14, word=16, accu=48, shift=11,
                 channel=3, profile=5)
-        self.submodules.proc = proc = iir.IIR(w)
+        self.submodules.proc = iir.IIR(proc_p)
 
-        for i, j in zip(adc.data, proc.adc):
-            self.comb += j.eq(i)
-
-        self.comb += [
-                proc.start.eq(adc.done),
-        ]
-
+        dds_p = dds_ser.DDSParams(width=8 + 32 + 16 + 16,
+                channels=adc_p.channels, clk=1)
         dds_pads = plat.request("dds_ser")
-        params = dds_ser.SPIParams(width=8 + 32 + 16 + 16, channels=8, clk=1)
-        self.submodules.dds = dds = dds_ser.SPIDDS(dds_pads, params)
+        self.submodules.dds = dds_ser.DDS(dds_pads, dds_p)
 
-        for i, j in zip(proc.dds, dds.profile):
-            self.comb += j.eq(i)
+        t_adc = (adc_p.t_cnvh + adc_p.t_conv + adc_p.t_rtt +
+            adc_p.channels*adc_p.width//2//adc_p.lanes)
+        t_iir = ((1 + 4 + 1) << proc_p.channel) + 1
+        t_dds = (dds_p.width*2 + 1)*dds_p.clk + 1
+        t_cycle = max(t_adc, t_iir, t_dds)
 
+        for i, j, k, l in zip(self.adc.data, self.proc.adc,
+                self.proc.dds, self.dds.profile):
+            self.comb += j.eq(i), l.eq(k)
+
+        self.start = Signal()
+        cnt = Signal(max=t_cycle)
+        cnt_done = Signal()
+        token = Signal(2)
+        self.done = Signal()
         self.comb += [
-                dds.start.eq(proc.done)
+                cnt_done.eq(cnt == 0),
+                self.adc.start.eq(self.start & cnt_done),
+                self.proc.start.eq(token[0] & self.adc.done),
+                self.dds.start.eq(token[1] & self.proc.shifting),
+                self.done.eq(self.dds.done),
+        ]
+        self.sync += [
+                If(~cnt_done,
+                    cnt.eq(cnt - 1),
+                ),
+                If(self.adc.done,
+                    token[0].eq(0),
+                ),
+                If(self.adc.start,
+                    cnt.eq(t_cycle - 1),
+                    token[0].eq(1)
+                ),
+                If(self.proc.shifting,
+                    token[1].eq(0),
+                ),
+                If(self.proc.start,
+                    token[1].eq(token[0]),
+                )
         ]
 
-        m_coeff = proc.m_coeff.get_port(write_capable=True)
-        m_state = proc.m_state.get_port(write_capable=True)
+        m_coeff = self.proc.m_coeff.get_port(write_capable=True)
+        m_state = self.proc.m_state.get_port(write_capable=True)
         self.specials += m_coeff, m_state
 
         ins = [
                 m_state.adr, m_state.we, m_state.dat_w,
                 m_coeff.adr, m_coeff.we, m_coeff.dat_w,
-                adc.start]
-        for ctrl in proc.ctrl:
+                self.start]
+        for ctrl in self.proc.ctrl:
             ins += ctrl.flatten()
-        self.dummy_inputs(ins, adc.start)
+        self.dummy_inputs(ins, self.start)
 
-        outs = [proc.shifting, proc.loading, proc.processing, proc.done,
-                m_state.dat_r, m_coeff.dat_r,
-                adc.reading, adc.done, dds.done]
-        self.dummy_outputs(outs, proc.done)
+        outs = [m_state.dat_r, m_coeff.dat_r]
+        self.dummy_outputs(outs, self.dds.done)
 
 
 if __name__ == "__main__":
