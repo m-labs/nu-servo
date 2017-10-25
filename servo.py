46710c5c26e32d43aee4f814a35c379b7333eba8
Robert Jordens <jordens@gmail.com>
2017-10-25 16:50:44 +0200

    servo: fix adc cycle duration

--- servo.py
+++ servo.py
@@ -16,7 +16,7 @@ class Servo(Module):
             self.comb += j.eq(i), l.eq(k)
 
         t_adc = (adc_p.t_cnvh + adc_p.t_conv + adc_p.t_rtt +
-            adc_p.channels*adc_p.width//2//adc_p.lanes)
+            adc_p.channels*adc_p.width//2//adc_p.lanes) + 1
         t_iir = ((1 + 4 + 1) << iir_p.channel) + 1
         t_dds = (dds_p.width*2 + 1)*dds_p.clk + 1
         t_cycle = max(t_adc, t_iir, t_dds)
